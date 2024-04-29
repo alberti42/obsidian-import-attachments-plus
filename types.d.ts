ee7689679f97f07f06015430a871bfcbf1a38a13
Andrea Alberti <a.alberti82@gmail.com>
2024-04-30 00:11:35 +0200

    Types

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+// types.d.ts
+
+// We can extend the File interface in TypeScript by declaring a global augmentation. This tells
+// TypeScript about additional properties that exist on the File object within our specific
+// Electron environment.
+
+declare global {
+  interface File {
+    path: string; // Add the path property to the File interface
+  }
+}
+
+export {};
---
+++ types.ts
@@ -0,0 +1,13 @@
+// types.ts
+
+export enum ImportActionType {
+	MOVE='MOVE',
+	COPY='COPY',
+	ASK_USER='ASK_USER'
+}
+
+export enum MultipleFilesImportTypes {
+	BULLETED='BULLETED',
+	NUMBERED='NUMBERED',
+	INLINE='INLINE'
+}
