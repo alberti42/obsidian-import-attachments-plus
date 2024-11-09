db1fff6e1611518ad58cf4495ddf5896f8833386
Andrea Alberti <a.alberti82@gmail.com>
2024-11-09 09:34:48 +0100

    eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    env: {
+        node: true, // Add this line to define Node.js global variables like `module`
+        es6: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    ignorePatterns: ['node_modules', 'dist', 'coverage', '**/*.d.ts', 'tests'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'interface',
+                format: ['PascalCase'],
+                custom: {
+                    regex: '^I[A-Z]',
+                    match: true
+                }
+            }
+        ],
+        '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/quotes': [
+            'error',
+            'single',
+            { avoidEscape: true, allowTemplateLiterals: false }
+        ],
+        curly: ['error', 'all'],
+        eqeqeq: 'error',
+        'prefer-arrow-callback': 'error'
+    }
+};
