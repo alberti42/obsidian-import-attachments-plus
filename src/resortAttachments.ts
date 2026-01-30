ee77708e8c6c235bead5d2e8477b251929dfd027
KraXen72 <dpelech1@gmail.com>
2026-01-30 23:06:55 +0100

    more regex stuff

--- src/resortAttachments.ts
+++ src/resortAttachments.ts
@@ -20,46 +20,56 @@ const	noteToFile = new Map<String, Set<TFile>>();
 export type SomeLink = {
 	text: string,
 	dest: string,
-	resolvedDest?: string
+	resolvedDest?: TFile
 }
 
-function cleanLinkDest(link: string) {
-	let link2 = link;
-	
-}
 
 function unifyLinkCaches(input: { f: TFile, m: CachedMetadata | null}) {
 	const links: SomeLink[] = [];
-	if (input.m) {
-		for (const elem of input.m?.links ?? []) {
-			if (!elem.original || elem.original.startsWith("[[#")) continue;
-			const res = app.metadataCache.getFirstLinkpathDest(elem.original, input.f.path); ;
-			console.log("res", res, "for", elem.original);
-			links.push({ text: elem.link, dest: elem.original });
+	if (!input.m) return { f: input.f, links: []};
+
+	for (const elem of [...(input.m?.links ?? []), ...(input.m?.frontmatterLinks ?? []) ]) {
+		if (!elem.original || elem.original.startsWith("[[#")) continue; // skip [[#heading]]
+
+		let dest = elem.original;
+		if (dest.startsWith("[[") && dest.endsWith("]]")) { // strip [[ and ]]
+			dest = dest.replace("[[", "").replace("]]", "");
 		}
-		for (const elem of input.m?.frontmatterLinks ?? []) {
-			if (!elem.original) continue;
-			links.push({ text: elem.link, dest: elem.original });
+		if (dest.match(/^\[.+\]\(.+\)$/)) continue; // skip links like [components](#components)
+		dest = dest.replace(/(?:\||\\\||#|\\#|).+$/, ""); // strip |alt, #heading, #heading|alt
+
+		const res: TFile | null = app.metadataCache.getFirstLinkpathDest(dest, input.f.path);
+		if (res == null) {
+			console.warn("could not resolve link:", elem.original, `(parsed as: '${dest}')`);
+			continue;
 		}
+		// we are not interested in notes linking to other notes
+		if (NOTE_EXTENSIONS.has(res.extension.toLowerCase())) continue;
+
+		links.push({ text: elem.link, dest: elem.original, resolvedDest: res });
 	}
 
 	return { f: input.f, links };
 }
 
 function buildReferenceMaps() {
-	const files = (app.vault.getFiles() as TFile[])
+	app.metadataCache.trigger('resolve');
+
+	const filesWithLinks = (app.vault.getFiles() as TFile[])
 		.filter(t => NOTE_EXTENSIONS.has(t.extension.toLowerCase()))
 		.map(t => ({ f: t, m: app.metadataCache.getFileCache(t) as CachedMetadata | null }))
 		.filter(e => e.m !== null && (
 			!(e.m.links == null || e.m.links.length === 0) ||
 			!(e.m.frontmatterLinks == null || e.m.frontmatterLinks.length === 0)
 		))
-		.slice(0, 100)
 		.map(unifyLinkCaches)
+		.filter(e => e.links.length > 0)
+		.slice(0, 10)
 
-	// for (const tup of files) {
-	// 	console.log(tup);
-	// }
+	for (const f of filesWithLinks) {
+		console.log(f);
+	}
+
 }
 
 export async function getAttachmentResortPairs(plugin: ImportAttachments) {
